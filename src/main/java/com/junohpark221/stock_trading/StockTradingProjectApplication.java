32fc4ccb6abb4f966f3b55c1de2d6a12a9ef5e0d
junohpark221 <junohpark221@gmail.com>
2024-11-10 13:54:13 +0900

    spring 초기 세팅 커밋

---
+++ src/main/java/com/junohpark221/stock_trading/StockTradingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.junohpark221.stock_trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTradingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTradingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junohpark221/stock_trading/StockTradingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junohpark221.stock_trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTradingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
